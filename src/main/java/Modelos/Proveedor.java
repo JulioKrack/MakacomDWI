059c678deebadd01848098cb99685feed5aee236
JulioKrack <julio.ruiz.dev@gmail.com>
2024-05-03 23:19:04 -0500

    se modifico el web.xml and add class proveedor

--- src/main/java/Modelos/Proveedor.java
+++ src/main/java/Modelos/Proveedor.java
@@ -1,5 +1,30 @@
 package Modelos;
 
-public class Proveedor {
+public class Proveedor extends Usuario {
+    private String marca;
+
+    public Proveedor() {
+    }
+
+    public Proveedor(String marca) {
+        this.marca = marca;
+    }
+
+    public Proveedor(String marca, String nombres, String apellidos, String correo, String contraseña, String rol) {
+        super(nombres, apellidos, correo, contraseña, rol);
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
     
+    
+        
+
+
 }
